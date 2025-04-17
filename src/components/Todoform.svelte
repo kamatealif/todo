906786fd83780d70afa9978b8f6796dc7a2f04e4
kamatealif <kamatealip1@gmail.com>
2025-04-17 21:30:32 +0530

    refactor todo form and page to use new task structure and improve functionality

--- src/components/Todoform.svelte
+++ src/components/Todoform.svelte
@@ -1,35 +1,36 @@
 <script>
-      
-      let todos = $state([{
-        name: null
-    }]);
-    let todoName = $state("")
-    let todoId = $state(0);
+  let newTask = $state("");
+  let { addTask } = $props();
 
-    const addTodo = () =>{
-        if(todoName && todoDesc){
-            todos.push({
-                id: todoId, 
-                name: todoName
-
-            })
-            todoId++;
-            todoName = "";
-        }
+  function addTodo(event) {
+    event.preventDefault();
+    if (newTask) {
+      addTask(newTask);
+      newTask = "";
     }
-
+  }
 </script>
 
-    <form action=""
-        onsubmit={addTodo}
-        class="flex justify-center items-start flex-col gap-y-1" >
-        <div class="flex flex-col gap-y-1">
-            <label for="todoname"
-                class="text-sm text-gray-500 dark:text-gray-400">Todo</label>
-            <input type="text" id="todoname" name="todoname" placeholder="Todo name" required
-            class="border border-gray-800 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent"
-            bind:value={todoName}> 
-        </div>
-    
-        <button type="submit" class="mt-4 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded" >Add Todo</button>
-    </form>
+<form
+  onsubmit={addTodo}
+  class="flex justify-center items-start flex-col gap-y-1"
+>
+  <label
+    for="todoname"
+    class="text-sm text-gray-500 dark:text-gray-400 flex flex-col gap-y-2"
+    >Todo
+    <input
+      type="text"
+      id="todoname"
+      name="todoname"
+      placeholder="Todo name"
+      required
+      class="border border-gray-800 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent"
+      bind:value={newTask}
+    />
+  </label>
+  <button
+    class=" text-white w-full bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded"
+    >Add Todo</button
+  >
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,25 @@
-<script>
-    import Listtodo from "./components/Listtodo.svelte";
-    import Todoform from "./components/Todoform.svelte";
-    let todos = $props()
+<script lang="ts">
+  // import Listtodo from "./components/Listtodo.svelte";
+  import Todoform from "../components/Todoform.svelte";
+  type Task = {
+    id: string;
+    title: string;
+    completed: boolean;
+  };
+  let tasks = $state<Task[]>([]);
+
+  function addTask(newTask) {
+    tasks.push({
+      id: crypto.randomUUID(),
+      title: newTask,
+      completed: false,
+    });
+  }
 </script>
 
 <main class="h-full w-full flex flex-col *:gap-y-4 justify-center items-center">
-    <Form />
-    <Listtodo {todos}/>
+  <Todoform {addTask} />
+  {#each tasks as task}
+    <p>{task.title}</p>
+  {/each}
 </main>
