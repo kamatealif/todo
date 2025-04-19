b103d97bf40507fd39925b453abeee393a57cb2d
kamatealif <kamatealip1@gmail.com>
2025-04-19 22:33:29 +0530

    refactor: update task display with total completed tasks and restore header styling

--- src/components/Listtodo.svelte
+++ src/components/Listtodo.svelte
@@ -2,9 +2,6 @@
   let { tasks, toggleDone } = $props();
 </script>
 
-<div class="hr-text-wrapper mb-2">
-  <div class="hr-text">Tasks</div>
-</div>
 <section class="mt-2">
   {#each tasks as task}
     <article>
@@ -23,32 +20,5 @@
 <style>
   .task-completed {
     text-decoration: line-through;
-  }
-  .hr-text-wrapper {
-    position: relative;
-    width: 100%;
-    text-align: center;
-    margin: 20px 0;
-  }
-
-  .hr-text {
-    position: relative;
-    display: inline-block;
-    padding: 0 10px;
-    font-size: 18px;
-    color: #e1d2e4;
-    background-color: #2d2d2d;
-  }
-
-  .hr-text-wrapper::before {
-    content: "";
-    position: absolute;
-    top: 50%;
-    left: 0;
-    width: 100%;
-    height: 1px;
-    background-color: #ffffff;
-    transform: translateY(-50%);
-    z-index: -1;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,9 @@
   };
   let tasks = $state<Task[]>([]);
 
+  let totalTasks = $derived(
+    tasks.reduce((total, task) => total + Number(task.completed), 0)
+  );
   function addTask(newTask) {
     tasks.push({
       id: crypto.randomUUID(),
@@ -25,5 +28,39 @@
   class="h-full max-w-[680px] mt-1 mx-auto flex flex-col *:gap-y-4 justify-center items-center"
 >
   <Todoform {addTask} />
+  <div class="hr-text-wrapper mb-2">
+    <div class="hr-text">Tasks</div>
+  </div>
+  <p>{totalTasks} / {tasks.length} Tasks Completed</p>
   <Listtodo {tasks} {toggleDone} />
 </main>
+
+<style>
+  .hr-text-wrapper {
+    position: relative;
+    width: 100%;
+    text-align: center;
+    margin: 20px 0;
+  }
+
+  .hr-text {
+    position: relative;
+    display: inline-block;
+    padding: 0 10px;
+    font-size: 18px;
+    color: #e1d2e4;
+    background-color: #2d2d2d;
+  }
+
+  .hr-text-wrapper::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: #ffffff;
+    transform: translateY(-50%);
+    z-index: -1;
+  }
+</style>
